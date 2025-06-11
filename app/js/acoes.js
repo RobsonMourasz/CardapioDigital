27adda88e6ed3686fc78ddee4a1c7b188096e4f1
RobsonMourasz <robsonic10@gmail.com>
2025-06-11 12:32:12 -0300

    refatora lógica de exibição de modal e adiciona eventos de carregamento ao DOM

--- app/js/acoes.js
+++ app/js/acoes.js
@@ -1,34 +1,7 @@
 (() => {
-    const modal = document.querySelectorAll('.close-modal');
-    modal.forEach(element => {
-        element.addEventListener('click', (e) => {
-            const parentDiv = e.target.closest('#pedido');
-            const iconeSacola = document.querySelector('.sacola-compras');
-            parentDiv.classList.add('d-none');
-            iconeSacola.classList.remove('d-none');
-        });
-    })
-
-    const modalSacola = document.querySelector('.sacola-compras');
-    modalSacola.addEventListener('click', () => {
-        const modalPedido = document.querySelector('#pedido');
-        modalPedido.classList.remove('d-none');
-        modalSacola.classList.add('d-none');
-    });
-
-    const card = document.querySelectorAll('.card');
-    card.forEach(element => {
-        element.addEventListener('click', (e) => {
-            console.log(e.target.closest('.card'));
-            const modalPedido = document.querySelector('#pedido');
-            const iconeSacola = document.querySelector('.sacola-compras');
-            modalPedido.classList.remove('d-none');
-            iconeSacola.classList.add('d-none');
-        })
-    });
 
     document.addEventListener('DOMContentLoaded', async () => {
-        document.querySelector(".carregando").classList.add("d-none");
+
         const response = await fetch('routes/api/produtos.php?busca=all');
         if (!response.ok) {
             console.error('Erro ao carregar os produtos');
@@ -38,6 +11,38 @@
         const produtos = await response.json();
         carregarTabela(produtos);
     });
+
+    document.addEventListener('load', async () => {
+        document.querySelector(".carregando").classList.add("d-none");
+        const modal = document.querySelectorAll('.close-modal');
+        modal.forEach(element => {
+            element.addEventListener('click', (e) => {
+                const parentDiv = e.target.closest('#pedido');
+                const iconeSacola = document.querySelector('.sacola-compras');
+                parentDiv.classList.add('d-none');
+                iconeSacola.classList.remove('d-none');
+            });
+        })
+    
+        const modalSacola = document.querySelector('.sacola-compras');
+        modalSacola.addEventListener('click', () => {
+            const modalPedido = document.querySelector('#pedido');
+            modalPedido.classList.remove('d-none');
+            modalSacola.classList.add('d-none');
+        });
+    
+        const card = document.querySelectorAll('.card');
+        console.log(card);
+        card.forEach(element => {
+            element.addEventListener('click', (e) => {
+                console.log(e.target.closest('.card'));
+                const modalPedido = document.querySelector('#pedido');
+                const iconeSacola = document.querySelector('.sacola-compras');
+                modalPedido.classList.remove('d-none');
+                iconeSacola.classList.add('d-none');
+            })
+        });
+    })
 
 })();
 
