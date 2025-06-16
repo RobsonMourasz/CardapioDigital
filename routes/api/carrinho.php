6d1943c28c19dcbb1c6a49dc73a2896f10059cbb
RobsonMourasz <robsonic10@gmail.com>
2025-06-16 18:56:43 -0300

    redireciona para URL após adicionar pedido e remove lógica de inserção de pedidos

--- routes/api/carrinho.php
+++ routes/api/carrinho.php
@@ -5,28 +5,29 @@ header('charset: UTF-8');
 
 if ( $_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['add'])){
-
+        $url = 'https://wa.me/34999918179';
         $ultimoPedido = src\class\Conexao::getPesquisaBD('SELECT MAX(a.idPedido) AS UltimoPedido FROM cadpedido a LIMIT	1', '', []);
         $proxPedido = $ultimoPedido++;
 
         $dadosPedido = [];
 
         if (isset($_POST)){
-            
-            foreach ($_POST['IdProduto'] as $key => $produtoId) {
-                $prod[] = src\class\Conexao::getPesquisaBD('SELECT * FROM cadprodutos a WHERE a.IdProduto = ?', 'i', [$produtoId]);
 
-                $valorItem = $prod['VrVenda'];
-                $qtd = $_POST['Quantidade'][$key];
-                $totalItem = $valorItem * $qtd ;
-                src\class\Conexao::insertBD("INSERT INTO cadpedido ('idPedido', 'ValorPedido', 'ValorEntrega', 'ValorAdicional', 'FormaPagamento', 'Controle', 'IpCliente', 'EnderecoEntrega', 'ObservacaoPedido', 'DataPedido' ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 'idddisssss', [$proxPedido, ]);
-                $dadosPedido[] = [
-                    'IdProduto' => $produtoId,
-                    'Quantidade' => $_POST['Quantidade'][$key],
-                    'ObsProduto' => $_POST['ObsProduto'][$key]
-                ];
-            }
+            // foreach ($_POST['IdProduto'] as $key => $produtoId) {
+            //     $prod[] = src\class\Conexao::getPesquisaBD('SELECT * FROM cadprodutos a WHERE a.IdProduto = ?', 'i', [$produtoId]);
+
+            //     $valorItem = $prod['VrVenda'];
+            //     $qtd = $_POST['Quantidade'][$key];
+            //     $totalItem = $valorItem * $qtd ;
+            //     src\class\Conexao::insertBD("INSERT INTO cadpedido ('idPedido', 'ValorPedido', 'ValorEntrega', 'ValorAdicional', 'FormaPagamento', 'Controle', 'IpCliente', 'EnderecoEntrega', 'ObservacaoPedido', 'DataPedido' ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 'idddisssss', [$proxPedido, ]);
+            //     $dadosPedido[] = [
+            //         'IdProduto' => $produtoId,
+            //         'Quantidade' => $_POST['Quantidade'][$key],
+            //         'ObsProduto' => $_POST['ObsProduto'][$key]
+            //     ];
+            // }
         }
+        header("Location: $url");
         http_response_code(200);
         die(json_encode(['status'=>'success','result' => 'Método encontrado! ']));
     }else{
