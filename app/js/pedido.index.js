0ab4ddde4ddbcac3eb0746f1ddebe979c5c6fb9b
RobsonMourasz <robsonic10@gmail.com>
2025-06-18 18:46:30 -0300

    adiciona cabeçalho e navegação responsiva à página de pedidos

---
+++ app/js/pedido.index.js
@@ -0,0 +1,12 @@
+(()=>{
+    document.querySelector('.toggle-menu .bi-list').addEventListener('click', ()=>{
+        document.querySelector('nav').classList.toggle('ocultar-toggle')
+    })
+
+    window.addEventListener('resize', ()=>{
+        const width = window.innerWidth;
+        if ( width <= 501 ) {
+            document.querySelector('nav').classList.add('ocultar-toggle')
+        }
+    })
+})();
--- public/pedido/index.php
+++ public/pedido/index.php
@@ -1,21 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../app/css/stylePedido.css">
     <title>Bem vindo</title>
 </head>
+
 <body>
     <header>
 
+        <div class="toggle-menu">
+            <i class="bi bi-list"></i>
+        </div>
+
+        <div class="logo-tipo">
+            <img src="../../app/assets/logo.png" alt="" >
+
+        </div>
+
+        <div class="informacoes-sistema">
+            Bem vindo ao sistema 
+        </div>
+
     </header>
+
     <nav>
 
     </nav>
+
     <section>
 
     </section>
-    <footer></footer>
+
+    <footer>
+
+    </footer>
+    <script src="../../app/js/pedido.index.js"></script>
 </body>
+
 </html>
