6e65dda8d5da064087333374dab06c23347cd029
RobsonMourasz <robsonic10@gmail.com>
2025-06-18 21:29:47 -0300

    adiciona estilo e estrutura à página 404, incluindo cabeçalho e corpo de conteúdo

--- public/pedido/page/404.php
+++ public/pedido/page/404.php
@@ -1,1 +1,9 @@
-asdasdas
+<div class="content">
+    <div class="content-header">
+        <h1>Oops página não encontrada!</h1>
+    </div>
+
+    <div class="content-body">
+        <p>Acredito que a pagina que está tentando procurar ou foi removida ou não existe, por favor contate o administrador</p>
+    </div>
+</div> <!-- content -->
