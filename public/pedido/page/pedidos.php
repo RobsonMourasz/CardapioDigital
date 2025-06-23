cb22c50dd58699f877e419a30c868f8790e594a9
RobsonMourasz <robsonic10@gmail.com>
2025-06-23 17:58:20 -0300

    adiciona novos cards de pedido e melhora a exibição do status com estilo flexível

--- public/pedido/page/pedidos.php
+++ public/pedido/page/pedidos.php
@@ -5,9 +5,9 @@
     <div class="content-body">
         <div class="conteiner">
 
-            <div class="card" pedido="1123123">
+            <div class="card" pedido="1">
                 <div class="card-header">
-                    <h3 class="title-pedido">Comanda: <span>Pedido:1123123</span></h3>
+                    <h3 class="title-pedido">Comanda: <span>Pedido:1</span></h3>
                 </div>
                 <div class="card-body">
                     <textarea name="itens-do-pedido" cols="22" rows="5" readonly>
@@ -18,6 +18,26 @@
                     <div class="footer-info">
                         <p class="status-pedido">Status: <span>Na chapa</span> </p>
                         <p>(Delivery)</p>
+                    </div>
+                    <div class="botao">
+                        <i class="btn bi bi-clipboard2-pulse-fill" id-modal="modal" attr="abrir"></i>
+                    </div>
+                </div>
+            </div> <!-- card -->
+
+            <div class="card" pedido="2">
+                <div class="card-header">
+                    <h3 class="title-pedido">Comanda: <span>Pedido:2</span></h3>
+                </div>
+                <div class="card-body">
+                    <textarea name="itens-do-pedido" cols="22" rows="5" readonly>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, minima.
+                    </textarea>
+                </div>
+                <div class="card-footer">
+                    <div class="footer-info">
+                        <p class="status-pedido">Status: <span>Na chapa</span> </p>
+                        <p>(Retirada)</p>
                     </div>
                     <div class="botao">
                         <i class="btn bi bi-clipboard2-pulse-fill" id-modal="modal" attr="abrir"></i>
@@ -36,13 +56,13 @@
             <span id-modal="modal" attr="fechar" aria-hidden="true">&#x2715;</span>
         </div>
         <div class="modal-body">
-            <div class="group-input btn-">
+            <div class="group-input btn-lista-ordenada">
                 <ul>
-                    <li><a href="">Iniciado</a></li>
-                    <li><a href="">Preparando</a></li>
-                    <li><a href="">Em rota</a></li>
-                    <li><a href="">Concluido</a></li>
-                    <li><a href="">Cancelado</a></li>
+                    <li><a class="text-align-center" href="">Iniciado</a></li>
+                    <li><a class="text-align-center" href="">Preparando</a></li>
+                    <li><a class="text-align-center" href="">Em rota</a></li>
+                    <li><a class="text-align-center" href="">Concluido</a></li>
+                    <li><a class="text-align-center" href="">Cancelado</a></li>
                 </ul>
             </div>
         </div>
