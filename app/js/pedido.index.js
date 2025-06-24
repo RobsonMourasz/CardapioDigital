0746bfe56b299de6b5b2864d393434419aaafce7
RobsonMourasz <robsonic10@gmail.com>
2025-06-24 12:32:57 -0300

    adiciona função chamarTelaAvisos para exibir mensagens de aviso com animações

--- app/js/pedido.index.js
+++ app/js/pedido.index.js
@@ -16,3 +16,32 @@
     }
     
 })();
+
+function chamarTelaAvisos(tipo,mensagem){
+    const telaAviso = document.getElementById('avisos');
+    const status = tipo.toLowerCase();
+    if (status == 'success'){
+        telaAviso.style.setProperty('--color-avisos', "#3b9a56");
+    }else if (status == 'danger'){
+        telaAviso.style.setProperty('--color-avisos', "#ff0c00");
+    }else{
+        telaAviso.style.setProperty('--color-avisos', "#22a0de");
+    }
+    telaAviso.classList.remove('d-none');
+    telaAviso.style.setProperty('--text-avisos', `"${mensagem}"`);
+    telaAviso.classList.remove('fade-out');
+    telaAviso.classList.add('fade-in')
+    setTimeout(() => {
+        telaAviso.classList.remove('fade-in')
+        telaAviso.classList.add('fade-out');
+
+        setTimeout(() =>{
+            telaAviso.classList.add('d-none');
+            telaAviso.style.setProperty('--text-avisos', "");
+            telaAviso.style.setProperty('--color-avisos', "");
+        },2000);
+
+        
+    }, 5000);
+    
+}
